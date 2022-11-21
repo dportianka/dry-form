2815df2dc2b0ad185adc75e6f2a5dd379d300774
Dmytro Portianka <dportianka@kayak.com>
2022-11-20 20:59:57 -0500

    uncontolled form example

--- src/Uncotrolled.js
+++ src/Uncotrolled.js
@@ -1,5 +1,52 @@
+/*
+ * Blazingly fast, no react udpates
+ * html standard
+ *
+ * Nobody knows html
+ * onChange updates
+ * inconsistnce some of the inputs should be treated differently
+ *
+ * */
+
 const Uncontrolled = () => {
-  return <div>unkontrolled</div>;
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+
+        console.log(
+          "Form value",
+          Object.fromEntries(
+            ["name", "email"].map((fieldName) => {
+              return [fieldName, event.target[fieldName].value];
+            })
+          )
+        );
+
+        console.log("Form values:", {
+          name: event.target.name.value,
+          email: event.target.email.value,
+          agree: event.target.agree.checked,
+        });
+      }}
+    >
+      <div>
+        <label htmlFor="name">Enter your name: </label>
+        <input type="text" name="name" id="name" required />
+      </div>
+      <div>
+        <label htmlFor="email">Enter your email: </label>
+        <input type="email" name="email" id="email" required />
+      </div>
+      <div>
+        <input type="checkbox" id="agree" name="agree" />
+        <label htmlFor="agree">Agree</label>
+      </div>
+      <div>
+        <input type="submit" value="Subscribe!" />
+      </div>
+    </form>
+  );
 };
 
 export default Uncontrolled;
